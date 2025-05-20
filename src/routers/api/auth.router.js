967a9e09a3670db5d9e403d15ad6be7c95a635a2
aguilardaniel <agdaniel89@gmail.com>
2025-05-20 20:56:36 -0300

    primera entrega

---
+++ src/routers/api/auth.router.js
@@ -0,0 +1,92 @@
+import { Router } from "express";
+import { usersManager } from "../../data/manager.mongo.js";
+//import passport from "../../middlewares/passport.mid.js";
+import passportCb from "../../middlewares/passportCb.mid.js";
+import { verifyToken } from "../../helpers/token.util.js";
+
+const authRouter = Router();
+
+const registerCb = async (req, res, next) => {
+  try {
+    const { method, originalUrl: url } = req;
+    const message = "Registered";
+    const data = { method, url, message };
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+const loginCb = async (req, res, next) => {
+  try {
+    const { method, originalUrl: url } = req;
+    const message = "Logged in";
+    const opts = { maxAge: 7 * 24 * 60 * 60 * 1000, signed: true };
+    const data = { method, url, message };
+    const { user } = req;
+    res.status(200).cookie("token", user.token, opts).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+const signoutCb = (req, res, next) => {
+  try {
+    const { method, originalUrl: url } = req;
+    const message = "Signed out";
+    /* eliminar la cookie y enviar respuesta al cliente */
+    const data = { method, url, message };
+    res.status(200).clearCookie("token").json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+const onlineCb = async (req, res, next) => {
+  try {
+    const { method, originalUrl: url } = req;
+    const { token } = req.signedCookies;
+    const dataToken = verifyToken(token);
+    let user = await usersManager.readById(dataToken?._id);
+    if (!user) {
+      const error = new Error("Invalid credentials");
+      error.statusCode = 401;
+      throw error;
+    }
+    const { password, __v, createdAt, updatedAt, ...rest } = user;
+    const data = {
+      method,
+      url,
+      user: rest,
+    };
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+const badAuthCb = (req, res, next) => {
+  try {
+    const error = new Error("Bad auth");
+    error.statusCode = 401;
+    throw error;
+  } catch (error) {
+    next(error);
+  }
+};
+const forbiddenCb = (req, res, next) => {
+  try {
+    const error = new Error("Forbidden");
+    error.statusCode = 403;
+    throw error;
+  } catch (error) {
+    next(error);
+  }
+};
+
+authRouter.post("/register", passportCb("register"), registerCb);
+authRouter.post("/login", passportCb("login"), loginCb);
+authRouter.post("/signout", passportCb("user"), signoutCb);
+authRouter.post("/online", passportCb("user"), onlineCb);
+authRouter.get("/bad-auth", badAuthCb);
+authRouter.get("/forbidden", forbiddenCb);
+authRouter.get("/google", passportCb("google", { scope: ["email", "profile"] }));
+authRouter.get("/google/redirect", passportCb("google"), loginCb);
+
+export default authRouter;
