127f5fe1d23183f9c8f4487231c4a1b3cad27663
aguilardaniel <agdaniel89@gmail.com>
2025-06-16 19:42:00 -0300

    Segunda Entrega

--- src/helpers/resetPassword.helper.js
+++ src/helpers/resetPassword.helper.js
@@ -6,6 +6,16 @@ import jwt from "jsonwebtoken";
 
 const resetPasswordEmail = async (resetToken) => {
 
+  let email;
+
+  try {
+    const payload = jwt.verify(resetToken, process.env.SECRET);
+    email= payload.email;
+  
+      } catch (err) {
+        return alert("Token inválido o expirado");
+      }
+
   await transport.sendMail({
     from: `EQUIPO CODER <${process.env.GOOGLE_EMAIL}>`,
     to: email,
@@ -15,10 +25,12 @@ const resetPasswordEmail = async (resetToken) => {
         <h1>ENLACE DE RESETEO DE CONTRASEÑA</h1>
         <h3>Ingrese aqui para poder restaurar su clave:</h3>
         <br>
-        /* <a href="${process.env.URL}/reset/${email}?token=${resetToken}">VERIFICAR</a> */
         <a href="${process.env.URL}/reset/${resetToken}">VERIFICAR</a>
       </section>
     `,
   });
+
+ 
+
 };
 export default resetPasswordEmail;
